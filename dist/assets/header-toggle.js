03a19ea812d3cc2228b38cf23b82359a1cee3165
aeternus36 <aeternus36@gmail.com>
2025-12-22 02:50:28 +0100

    Update header-toggle.js

--- dist/assets/header-toggle.js
+++ dist/assets/header-toggle.js
@@ -1,15 +1,10 @@
 // header-toggle.js
+
 document.addEventListener("DOMContentLoaded", function () {
   const toggle = document.getElementById("menuToggle");
   const menu = document.getElementById("mainnav");
 
-  toggle.addEventListener("click", () => {
+  toggle.addEventListener("click", function () {
     menu.classList.toggle("active");
-  });
-
-  document.addEventListener("click", (e) => {
-    if (!menu.contains(e.target) && !toggle.contains(e.target)) {
-      menu.classList.remove("active");
-    }
   });
 });
